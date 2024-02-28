cf89fe3ba1ba00eed26c1c610afd5e9bb719107b
Pravin Kamble <pravinkamble15501@gmail.com>
2024-02-28 13:45:16 +0530

    Third shell

---
+++ Script4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#This is creation and calling of shell variable --- defining variables
+Class=Young-minds
+Batch=13
+PROFESSION=AWS/DevOps
+Company=TCS
+echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
+echo "Company Name $Company"
